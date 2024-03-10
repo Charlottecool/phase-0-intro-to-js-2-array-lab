f2d3cd404471f209bdbe5b23f06c82e79e6e5eb1
Charlotte Sun <suncharlotte7@gmail.com>
2024-03-10 18:15:30 -0400

    pass all the tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (name) { 
+    name = cats.push('Ralph');
+}
+function destructivelyPrependCat (name) { 
+    name = cats.unshift('Bob');
+}
+function destructivelyRemoveLastCat () { 
+    cats.pop();
+}
+function destructivelyRemoveFirstCat () { 
+    cats.shift();
+}
+function appendCat (name) { 
+    name = 'Broom';
+    return [...cats, name];
+}
+function prependCat (name) { 
+    name = 'Arnold';
+    return [name, ...cats];
+}
+function removeLastCat () { 
+    return cats.slice(0, cats.length - 1);
+}
+function removeFirstCat () { 
+    return cats.slice(1);
+}
